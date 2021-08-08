d2b3ba2324f102b325ad0ef55ca9640f41c84079
vishwajitmalusare <vishwajitmalusare@gmail.com>
2021-08-09 00:26:47 +0530

    Add. ablet to display data from api

--- src/component/Countries.jsx
+++ src/component/Countries.jsx
@@ -1,34 +1,65 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import './Countries.css'
 
 const url = "https://restcountries.eu/rest/v2/region/asia"
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
 
-
-    const fetchCountryData = async() => {
-        const response = await fetch(url)
-        const countries = await response.json()
-        setCountries(countries)
-        console.log(countries)
-    }
-
     useEffect(() => {
+        const fetchCountryData = async () => {
+            const response = await fetch(url)
+            const countries = await response.json()
+            setCountries(countries)
+            //console.log(countries)
+        }
+
         fetchCountryData()
     }, [])
+    
+    
 
     return (
-        <div>
-            <h1>Display Country Data</h1>
-            
+        <>            
+        <section classsName="grid">
             {countries.map((country) => {
                 
-                const{ name, region, subregion, population, capital, borders, languages } = country
+                const { numericCode,
+                      name,
+                     flag,
+                     region,
+                     subregion,
+                     population,
+                     capital,
+                     borders,
+                     languages, 
+                } = country
 
-                return <article key={id}>Country Data</article>
+                return <article key={numericCode}>
+                    <div classsName="grid-item">
+                        <img src={flag} alt={name}/>
+                        <h3> Country Name: <span> {name} </span> </h3>
+                        <h4>Capital:  <span> {capital} </span> </h4>
+                        <h4> Region: <span> {region} </span> </h4>
+                        <h4> SubRegion: <span> {subregion} </span> </h4>
+                        <h4> Language: <span> {languages[0].name} </span>
+                        </h4>
+                        <h4> Borders: 
+                            {borders.map((border) => {
+                                return (
+                                    <ul key={border}>
+                                        {border}
+                                    </ul>
+                                )
+                            })}
+                        </h4>
+                        <h4> Population: <span> {population} </span></h4>
+                    </div>
+                </article>
             })}
-        </div>
+            </section>
+        </>
     )
 }
 
